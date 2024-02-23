1a96527c513bbc505dbff2ac51c38a1d196e0ab2
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-23 09:38:08 +0100

    Add data file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
 import './App.css'
-
+import text from "./data/data.js"
+import NumberInput from './components/NumberInput.js'
 function App() {
 
   return (
     <>
-      
+      {<div className='bg-red-500'>{text}</div>}
+      {/* <NumberInput/> */}
     </>
   )
 }
---
+++ src/components/NumberInput.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NumberInput = () => {
+  return (
+<>
+    {/* <input type="number" value={1} /> */}
+</>      
+  )
+}
+
+export default NumberInput
