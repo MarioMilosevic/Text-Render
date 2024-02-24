85fae4b4ba41ba4e18d978e51dc595a33cb5a1d8
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-24 17:03:45 +0100

    Add useEffect Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
 import "./App.css";
 import { text as dataText } from "./data/data.ts";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NumberInput from "./components/NumberInput.js";
 import TextField from "./components/TextField.js";
 import Heading from "./components/Heading.js";
 
 function App() {
-  const [value, setValue] = useState<number>(0);
+  const [value, setValue] = useState<number>(1);
   const [text, setText] = useState(dataText);
-  const [paragraphs, setParagraphs] = useState([])
+  const [paragraphs, setParagraphs] = useState([]);
 
+  useEffect(() => {
+    const slicedArray = text.slice(0, value);
+    setParagraphs(slicedArray);
+  }, [value]);
+  
   const changeValue = (e) => {
     const target = Number(e.target.value);
-    changeText(target)
     setValue(target);
-  };
-  const changeText = (value: number) => {
-    const slicedArray = text.slice(0, value);
-    setParagraphs(slicedArray)
-    return slicedArray;
   };
 
   return (
     <>
       <Heading />
       <NumberInput value={value} changeValue={changeValue} />
-      {paragraphs.map(paragraph => {
-        return <TextField text={paragraph} key={paragraph}/>
+      {paragraphs.map((paragraph) => {
+        return <TextField text={paragraph} key={paragraph} />;
       })}
     </>
   );
